dcd4c5cc1a6c3a38d85e07e9e0fdb95ed46e5361
alcatel4 <m.gonzalez.13@estudiantec.cr>
2026-03-20 10:34:15 -0600

    ubicaciones musicales y calculo de distancia

--- app/src/main/java/com/example/geobeats/MostrarMapa.kt
+++ app/src/main/java/com/example/geobeats/MostrarMapa.kt
@@ -8,9 +8,10 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import android.content.Context
+import android.widget.Toast
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 
 @Composable
-// Recibe la ubicación del usuario desde MainActivity
 fun MapScreen(userLocation: LatLng?) {
 
     AndroidView(
@@ -23,10 +24,9 @@ fun MapScreen(userLocation: LatLng?) {
 
             mapView.getMapAsync { googleMap: GoogleMap ->
 
-                // Habilita controles de zoom
                 googleMap.uiSettings.isZoomControlsEnabled = true
 
-                // Si tenemos ubicación del usuario, centramos el mapa y agregamos marcador
+
                 userLocation?.let {
 
                     googleMap.moveCamera(
@@ -39,9 +39,60 @@ fun MapScreen(userLocation: LatLng?) {
                             .title("Tu ubicación")
                     )
                 }
+
+
+                val basilica = LatLng(9.8644, -83.9194)
+
+                googleMap.addMarker(
+                    MarkerOptions()
+                        .position(basilica)
+                        .title("Zona musical")
+                        .icon(
+                            BitmapDescriptorFactory.defaultMarker(
+                            BitmapDescriptorFactory.HUE_BLUE
+                        ))
+                )
+
+
+                googleMap.setOnMarkerClickListener { marker ->
+                    Toast.makeText(context,marker.title, Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+
+                userLocation?.let {
+
+                    val distancia = calcularDistancia(
+                        it.latitude, it.longitude,
+                        basilica.latitude, basilica.longitude
+                    )
+
+                    if (distancia < 100) {
+                        Toast.makeText(context, "Entraste a la zona musical", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
 
             mapView
         }
     )
+}
+fun calcularDistancia(
+    lat1: Double, lon1: Double,
+    lat2: Double, lon2: Double
+): Double {
+
+    val R = 6371000.0
+
+    val dLat = Math.toRadians(lat2 - lat1)
+    val dLon = Math.toRadians(lon2 - lon1)
+
+    val a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(Math.toRadians(lat1)) *
+            Math.cos(Math.toRadians(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2)
+
+    val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    return R * c
 }
